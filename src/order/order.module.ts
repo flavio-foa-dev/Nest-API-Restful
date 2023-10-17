79a22fea7e0d91e3ec0cc4873fd20a35a517baaf
Flavio Andrade <foa12381@gmail.com>
2023-10-17 13:01:12 -0300

    create/order/product

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -4,9 +4,10 @@ import { OrderController } from './order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
 import { UserEntity } from '../user/entities/user.entity';
+import { ProductEntity } from 'src/product/entities/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([OrderEntity, UserEntity, ProductEntity])],
   controllers: [OrderController],
   providers: [OrderService],
 })
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderEntity } from './entities/order.entity';
-import { Repository } from 'typeorm';
-import { UserEntity } from 'src/user/entities/user.entity';
+import { In, Repository } from 'typeorm';
+import { UserEntity } from '../user/entities/user.entity';
 import { OrderStatus } from './enum/orderStatus';
 import { UUID } from 'crypto';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderItemEntity } from './entities/order-item.entity';
+import { ProductEntity } from '../product/entities/product.entity';
 
 @Injectable()
 export class OrderService {
@@ -15,15 +16,28 @@ export class OrderService {
     private readonly orderRepository: Repository<OrderEntity>,
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
   ) {}
 
   async create(userId: UUID, data: CreateOrderDto) {
     const user = await this.userRepository.findOneBy({ id: userId });
+    const productsIds = data.orderItems.map((item) => item.productId);
+
+    const productsRelations = await this.productRepository.findBy({
+      id: In(productsIds),
+    });
 
     const itemsOrderEntity = data.orderItems.map((item) => {
+      const product = productsRelations.find(
+        (product) => product.id === item.productId,
+      );
       const itemOrderEntity = new OrderItemEntity();
-      itemOrderEntity.price = 10;
+      itemOrderEntity.product = product;
+      itemOrderEntity.price = product.price;
       itemOrderEntity.quantity = item.quantity;
+
+      itemOrderEntity.product.stock -= item.quantity;
       return itemOrderEntity;
     });
 
