18992bed394732209bfe00fe164141ca7a666e04
Flavio Andrade <foa12381@gmail.com>
2023-10-21 21:23:16 -0300

    add guar routes orders

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,8 +1,25 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
   canActivate(context: ExecutionContext): boolean {
-    return false;
+    const request = context.switchToHttp().getRequest();
+    const token = this.getToken(request);
+    console.log('tokeneeeeee', token);
+    if (!token) throw new UnauthorizedException('Error autentication');
+    return true;
+  }
+  private getToken(request: Request): string | undefined {
+    const [type, payload] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? payload : undefined;
   }
 }
